c1d6e2e7b62e9b8a2c95efdc3f96e8b8c69f09f4
samwanyua <samexample8@gmail.com>
2024-06-07 12:26:39 +0300

    Create ingredients model

--- Samfood/src/main/java/com/wanyua/Samfood/Model/Food.java
+++ Samfood/src/main/java/com/wanyua/Samfood/Model/Food.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -33,4 +34,10 @@ public class Food {
 
     @ManyToOne
     private Restaurant restaurant;
+
+    private boolean isVegetarian;
+
+    private boolean isSeasonal;
+
+    private List<Ingredients> ingredients = new ArrayList<>();
 }
---
+++ Samfood/src/main/java/com/wanyua/Samfood/Model/Ingredients.java
@@ -0,0 +1,19 @@
+package com.wanyua.Samfood.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ingredients {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
