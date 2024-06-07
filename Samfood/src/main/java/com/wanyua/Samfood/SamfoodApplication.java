a96e4a5b18af01a654f9ec6fadb199a344be7d73
samwanyua <samexample8@gmail.com>
2024-06-07 11:19:47 +0300

    Initial commit

---
+++ Samfood/src/main/java/com/wanyua/Samfood/SamfoodApplication.java
@@ -0,0 +1,13 @@
+package com.wanyua.Samfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamfoodApplication.class, args);
+	}
+
+}
---
+++ Samfood/src/test/java/com/wanyua/Samfood/SamfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanyua.Samfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
